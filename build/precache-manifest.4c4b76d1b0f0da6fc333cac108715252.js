fa8980b1f12704fbcb56c786e709a3c6bd435d14
khansameer295 <khan.sameer295@gmail.com>
2020-10-27 15:17:04 +0530

    Build added

---
+++ build/precache-manifest.4c4b76d1b0f0da6fc333cac108715252.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "092f93b5682ac5bd45564277db0d46bc",
+    "url": "/efrontliners/efrs-test/index.html"
+  },
+  {
+    "revision": "304d58302dbab26e9d2a",
+    "url": "/efrontliners/efrs-test/static/css/main.66710925.chunk.css"
+  },
+  {
+    "revision": "4de98fc1c1dccd55f364",
+    "url": "/efrontliners/efrs-test/static/js/2.ec714286.chunk.js"
+  },
+  {
+    "revision": "e118979dd0cbb59f8ac000edcc8fb6d1",
+    "url": "/efrontliners/efrs-test/static/js/2.ec714286.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "304d58302dbab26e9d2a",
+    "url": "/efrontliners/efrs-test/static/js/main.cfaae613.chunk.js"
+  },
+  {
+    "revision": "98a6fabbb97881b0e8d1",
+    "url": "/efrontliners/efrs-test/static/js/runtime-main.6903a370.js"
+  },
+  {
+    "revision": "f9977eab6a361b5c8c12f1f0b07295ab",
+    "url": "/efrontliners/efrs-test/static/media/add.f9977eab.png"
+  },
+  {
+    "revision": "304e9f8f6698ad73eaa320815121c4f3",
+    "url": "/efrontliners/efrs-test/static/media/addPhoto.304e9f8f.png"
+  },
+  {
+    "revision": "06f226fc9fce2e584bcaab79c4d19ad1",
+    "url": "/efrontliners/efrs-test/static/media/company.06f226fc.png"
+  },
+  {
+    "revision": "f233a79a13462b29d6efaa57daaf97ab",
+    "url": "/efrontliners/efrs-test/static/media/designation.f233a79a.png"
+  },
+  {
+    "revision": "90179932e7033bddf4f43ad2197ed784",
+    "url": "/efrontliners/efrs-test/static/media/edit.90179932.png"
+  },
+  {
+    "revision": "b8d582f5f0f9bf03d9d0ea9b4c746a79",
+    "url": "/efrontliners/efrs-test/static/media/gender.b8d582f5.png"
+  },
+  {
+    "revision": "e8597c1c06333c5faeac47dc36894edf",
+    "url": "/efrontliners/efrs-test/static/media/jobs.e8597c1c.png"
+  },
+  {
+    "revision": "d7a5b4829b45fe32432b135767034a81",
+    "url": "/efrontliners/efrs-test/static/media/location.d7a5b482.png"
+  },
+  {
+    "revision": "2dbe209c208ee5b58102820a1059340c",
+    "url": "/efrontliners/efrs-test/static/media/logosplash.2dbe209c.png"
+  },
+  {
+    "revision": "d8ae387cd91b56652a773fb51f92421f",
+    "url": "/efrontliners/efrs-test/static/media/logout.d8ae387c.png"
+  },
+  {
+    "revision": "30bf112ae9b94b235dac3f6a88427686",
+    "url": "/efrontliners/efrs-test/static/media/office.30bf112a.png"
+  },
+  {
+    "revision": "47fcdee126296a4bc451920137504655",
+    "url": "/efrontliners/efrs-test/static/media/phone.47fcdee1.png"
+  },
+  {
+    "revision": "394f0bff0311d21096e2d64a2d2dd51f",
+    "url": "/efrontliners/efrs-test/static/media/profession.394f0bff.png"
+  },
+  {
+    "revision": "9b9326c35875cdbe173ab726b39ab6e6",
+    "url": "/efrontliners/efrs-test/static/media/recruiter.9b9326c3.png"
+  }
+]);
